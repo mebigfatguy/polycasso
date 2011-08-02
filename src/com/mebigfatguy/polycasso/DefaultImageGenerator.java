81832239983d0c7aacb88f8772a753123d82897b
Dave Brosius <dbrosius@mebigfatguy.com>
2011-08-01 23:16:55 -0400

    add annotations

--- src/com/mebigfatguy/polycasso/DefaultImageGenerator.java
+++ src/com/mebigfatguy/polycasso/DefaultImageGenerator.java
@@ -4,17 +4,17 @@
  * Copyright 2009-2011 Dave Brosius
  * Inspired by work by Roger Alsing
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- *    http://www.apache.org/licenses/LICENSE-2.0 
- *    
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and limitations 
- * under the License. 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
  */
 package com.mebigfatguy.polycasso;
 
@@ -33,242 +33,253 @@ import java.util.Set;
 
 /**
  * class that generates test images iteratively looking for the best image that matches a target.
- * The images are generated from semi-transparent polygons that are improved upon over time. 
+ * The images are generated from semi-transparent polygons that are improved upon over time.
  * This class generates multiple images in parallel to keep multicore processors busy.
  */
 public class DefaultImageGenerator implements ImageGenerator, Runnable {
-	private Set<ImageGeneratedListener> listeners = new HashSet<ImageGeneratedListener>();
-	private Settings settings;
-	private BufferedImage targetImage; 
-	private GenerationHandler generationHandler;
-	private Dimension imageSize;
-	private Feedback feedback;
-	private Thread[] t = null;
-	private Object startStopLock = new Object();
-	
-	/**
-	 * creates an ImageGenerator for the given target image, and size
-	 * @param confSettings the configuration settings
-	 * @param image the target image
-	 * @param size the dimension of the image
-	 */
-	public DefaultImageGenerator(Settings confSettings, Image image, Dimension size) {
-		settings = confSettings;
-		imageSize = trimSize(size, settings.getMaxImageSize());
-		targetImage = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
-		
-		Graphics g = targetImage.getGraphics();
-		try {
-    		g.drawImage(image, 0, 0, imageSize.width, imageSize.height, Color.WHITE, null);
-    		generationHandler = new GenerationHandler(settings, imageSize);
-    		feedback = new DefaultFeedback();
-    		feedback.setTargetImage(targetImage);
-		} finally {
-		    g.dispose();
-		}
-	}
-	
-	/**
-	 * retrieves the scaled target iamge
-	 * 
-	 * @return the target image
-	 */
-	public BufferedImage getTargetImage() {
-		return targetImage;
-	}
-	
-	/**
-	 * returns the image size that is being generated. This size might be different the original image
-	 * if the size is bigger then the max setting.
-	 * 
-	 * @return the image size
-	 */
-	public Dimension getImageSize() {
-		return imageSize;
-	}
-	
-	/**
-	 * allows interested parties to register to receive events when a new best image has been
-	 * found.
-	 * 
-	 * @param listener the listener that is interested in events
-	 */
-	public void addImageGeneratedListener(ImageGeneratedListener listener) {
-		listeners.add(listener);
-	}
-	
-	/**
-	 * allows uninterested parties to unregister to receive events when a new best image is 
-	 * found
-	 * 
-	 * @param listener the listener that is no longer needed
-	 */
-	public void removeImageGeneratedListener(ImageGeneratedListener listener) {
-		listeners.remove(listener);
-	}
-	
-	/**
-	 * informs all listeners that a new best image has been found
-	 * 
-	 * @param image the new best image
-	 */
-	public void fireImageGenerated(Image image) {
-		ImageGeneratedEvent event = new ImageGeneratedEvent(this, image);
-		for (ImageGeneratedListener listener : listeners) {
-			listener.imageGenerated(event);
-		}
-	}
-	
-	/**
-	 * starts up threads to start looking for images that are closest to the target
-	 */
-	public void startGenerating() {
-		synchronized(startStopLock) {
-			if (t == null) {
-			    
-			    populateGenerationZeroElite();
-				t = new Thread[Runtime.getRuntime().availableProcessors() + 1];
-				for (int i = 0; i < t.length; i++) {
-					t[i] = new Thread(this);
-					t[i].setName("Improver : " + i);
-					t[i].start();
-				}
-			}
-		}
-	}
-	
-	/**
-	 * shuts down threads that were looking for images
-	 */
-	public void stopGenerating() {
-		synchronized(startStopLock) {
-			if (t != null) {
-				try {
-					for (int i = 0; i < t.length; i++) {
-						t[i].interrupt();
-					}
-					for (int i = 0; i < t.length; i++) {
-						t[i].join();
-					}
-				} catch (InterruptedException ie) {
-				} finally {
-					t = null;
-				}
-			}
-		}
-	}
-	
-	/**
-	 * completes the image by transforming the polygon image to the real image
-	 */
-	public void complete() {
-		synchronized(startStopLock) {
-			if (t != null) {
-				stopGenerating();
-				t = new Thread[1];
-				t[0] = new Thread(new ImageCompleter(this, targetImage, generationHandler.getBestMember().getData(), imageSize));
-				t[0].start();
-			}
-		}
-	}
-	
-	/**
-	 * retrieves the best set of polygons for drawing the image so far
-	 * 
-	 * @return the best set of polygons
-	 */
-	public PolygonData[] getBestData() {
-		return generationHandler.getBestMember().getData();
-	}
-	/**
-	 * the runnable interface implementation to repeatedly improve upon the image and check to 
-	 * see if it is closer to the target image. Images are created in batches of settings.numCompetingImages
-	 * and the best one (if better than the parent) is selected as the new best.
-	 */
-	public void run() {
-		try {
-			BufferedImage image = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
-			Graphics2D g2d = (Graphics2D)image.getGraphics();
-			try {
-    			Composite srcOpaque = AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f);
-    			Improver improver = new Improver(settings, generationHandler, imageSize);
-    			
-    			while (!Thread.interrupted()) {
-    				ImprovementType type = improver.improveRandomly();
-    				
-    				List<PolygonData> data = improver.getData();
-    				imagePolygonData(g2d, data, srcOpaque);
-    
-    				GenerationMember parentMember = improver.getParentGenerationMember();
-    				Score delta = feedback.calculateScore(image, (parentMember != null) ? parentMember.getScore() : null, improver.getChangedArea());
-    				
-    				boolean wasSuccessful;
-    				
-    				ImprovementResult result = generationHandler.addPolygonData(delta, data.toArray(new PolygonData[data.size()]));
-    				switch (result) {
-    					case BEST:
-    						fireImageGenerated(image);
-    						wasSuccessful = true;	
-    						image = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
-    						g2d.dispose();
-    						g2d = (Graphics2D)image.getGraphics();
-    					break;
-    					
-    					case ELITE:
-    						wasSuccessful = true;
-    					break;
-    					
-    					default:
-    						wasSuccessful = false;
-    				}
-    				
-    				improver.typeWasSuccessful(type, wasSuccessful);
-    			}
-			} finally {
-			    g2d.dispose();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void imagePolygonData(Graphics2D g2d, List<PolygonData> polygonData, Composite srcOpaque) {
-	    g2d.setColor(Color.BLACK);
+    private final Set<ImageGeneratedListener> listeners = new HashSet<ImageGeneratedListener>();
+    private final Settings settings;
+    private final BufferedImage targetImage;
+    private GenerationHandler generationHandler;
+    private final Dimension imageSize;
+    private Feedback feedback;
+    private Thread[] t = null;
+    private final Object startStopLock = new Object();
+
+    /**
+     * creates an ImageGenerator for the given target image, and size
+     * @param confSettings the configuration settings
+     * @param image the target image
+     * @param size the dimension of the image
+     */
+    public DefaultImageGenerator(Settings confSettings, Image image, Dimension size) {
+        settings = confSettings;
+        imageSize = trimSize(size, settings.getMaxImageSize());
+        targetImage = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
+
+        Graphics g = targetImage.getGraphics();
+        try {
+            g.drawImage(image, 0, 0, imageSize.width, imageSize.height, Color.WHITE, null);
+            generationHandler = new GenerationHandler(settings, imageSize);
+            feedback = new DefaultFeedback();
+            feedback.setTargetImage(targetImage);
+        } finally {
+            g.dispose();
+        }
+    }
+
+    /**
+     * retrieves the scaled target iamge
+     * 
+     * @return the target image
+     */
+    @Override
+    public BufferedImage getTargetImage() {
+        return targetImage;
+    }
+
+    /**
+     * returns the image size that is being generated. This size might be different the original image
+     * if the size is bigger then the max setting.
+     * 
+     * @return the image size
+     */
+    @Override
+    public Dimension getImageSize() {
+        return imageSize;
+    }
+
+    /**
+     * allows interested parties to register to receive events when a new best image has been
+     * found.
+     * 
+     * @param listener the listener that is interested in events
+     */
+    @Override
+    public void addImageGeneratedListener(ImageGeneratedListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * allows uninterested parties to unregister to receive events when a new best image is
+     * found
+     * 
+     * @param listener the listener that is no longer needed
+     */
+    @Override
+    public void removeImageGeneratedListener(ImageGeneratedListener listener) {
+        listeners.remove(listener);
+    }
+
+    /**
+     * informs all listeners that a new best image has been found
+     * 
+     * @param image the new best image
+     */
+    @Override
+    public void fireImageGenerated(Image image) {
+        ImageGeneratedEvent event = new ImageGeneratedEvent(this, image);
+        for (ImageGeneratedListener listener : listeners) {
+            listener.imageGenerated(event);
+        }
+    }
+
+    /**
+     * starts up threads to start looking for images that are closest to the target
+     */
+    @Override
+    public void startGenerating() {
+        synchronized(startStopLock) {
+            if (t == null) {
+
+                populateGenerationZeroElite();
+                t = new Thread[Runtime.getRuntime().availableProcessors() + 1];
+                for (int i = 0; i < t.length; i++) {
+                    t[i] = new Thread(this);
+                    t[i].setName("Improver : " + i);
+                    t[i].start();
+                }
+            }
+        }
+    }
+
+    /**
+     * shuts down threads that were looking for images
+     */
+    @Override
+    public void stopGenerating() {
+        synchronized(startStopLock) {
+            if (t != null) {
+                try {
+                    for (Thread element : t) {
+                        element.interrupt();
+                    }
+                    for (Thread element : t) {
+                        element.join();
+                    }
+                } catch (InterruptedException ie) {
+                } finally {
+                    t = null;
+                }
+            }
+        }
+    }
+
+    /**
+     * completes the image by transforming the polygon image to the real image
+     */
+    @Override
+    public void complete() {
+        synchronized(startStopLock) {
+            if (t != null) {
+                stopGenerating();
+                t = new Thread[1];
+                t[0] = new Thread(new ImageCompleter(this, targetImage, generationHandler.getBestMember().getData(), imageSize));
+                t[0].start();
+            }
+        }
+    }
+
+    /**
+     * retrieves the best set of polygons for drawing the image so far
+     * 
+     * @return the best set of polygons
+     */
+    @Override
+    public PolygonData[] getBestData() {
+        return generationHandler.getBestMember().getData();
+    }
+    /**
+     * the runnable interface implementation to repeatedly improve upon the image and check to
+     * see if it is closer to the target image. Images are created in batches of settings.numCompetingImages
+     * and the best one (if better than the parent) is selected as the new best.
+     */
+    @Override
+    public void run() {
+        try {
+            BufferedImage image = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
+            Graphics2D g2d = (Graphics2D)image.getGraphics();
+            try {
+                Composite srcOpaque = AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f);
+                Improver improver = new Improver(settings, generationHandler, imageSize);
+
+                while (!Thread.interrupted()) {
+                    ImprovementType type = improver.improveRandomly();
+
+                    List<PolygonData> data = improver.getData();
+                    imagePolygonData(g2d, data, srcOpaque);
+
+                    GenerationMember parentMember = improver.getParentGenerationMember();
+                    Score delta = feedback.calculateScore(image, (parentMember != null) ? parentMember.getScore() : null, improver.getChangedArea());
+
+                    boolean wasSuccessful;
+
+                    ImprovementResult result = generationHandler.addPolygonData(delta, data.toArray(new PolygonData[data.size()]));
+                    switch (result) {
+                        case BEST:
+                            fireImageGenerated(image);
+                            wasSuccessful = true;
+                            image = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
+                            g2d.dispose();
+                            g2d = (Graphics2D)image.getGraphics();
+                            break;
+
+                        case ELITE:
+                            wasSuccessful = true;
+                            break;
+
+                        default:
+                            wasSuccessful = false;
+                    }
+
+                    improver.typeWasSuccessful(type, wasSuccessful);
+                }
+            } finally {
+                g2d.dispose();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void imagePolygonData(Graphics2D g2d, List<PolygonData> polygonData, Composite srcOpaque) {
+        g2d.setColor(Color.BLACK);
         g2d.setComposite(srcOpaque);
         g2d.fillRect(0, 0, imageSize.width, imageSize.height);
-        
+
         for (PolygonData pd : polygonData) {
             pd.draw(g2d);
         }
-	}
-	
-	private void populateGenerationZeroElite() {
+    }
+
+    private void populateGenerationZeroElite() {
         Composite srcOpaque = AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f);
-	    for (int i = 0; i < settings.getEliteSize(); i++) {   
-	        BufferedImage image = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
-	        List<PolygonData> polygons = new ArrayList<PolygonData>();
-	        PolygonData pd = PolygonData.randomPoly(imageSize, settings.getMaxPoints());
-	        polygons.add(pd);
-	        Graphics2D g2d = (Graphics2D)image.getGraphics();
-	        try {
+        for (int i = 0; i < settings.getEliteSize(); i++) {
+            BufferedImage image = new BufferedImage(imageSize.width, imageSize.height, BufferedImage.TYPE_4BYTE_ABGR);
+            List<PolygonData> polygons = new ArrayList<PolygonData>();
+            PolygonData pd = PolygonData.randomPoly(imageSize, settings.getMaxPoints());
+            polygons.add(pd);
+            Graphics2D g2d = (Graphics2D)image.getGraphics();
+            try {
                 imagePolygonData(g2d, polygons, srcOpaque);
                 Score delta = feedback.calculateScore(image, null, null);
                 generationHandler.addPolygonData(delta, polygons.toArray(new PolygonData[polygons.size()]));
-	        } finally {
-	            g2d.dispose();
-	        }
-	    }
-	}
-	
-	private Dimension trimSize(Dimension origSize, Dimension maxSize) {
-		if ((origSize.width < maxSize.width) && (origSize.height < maxSize.height))
-			return origSize;
-		
-		double hFrac = (double)maxSize.width / (double)origSize.width;
-		double vFrac = (double)maxSize.height/ (double)origSize.height;
-		
-		double frac = (hFrac < vFrac) ? hFrac : vFrac;
-		
-		return new Dimension((int)(frac * origSize.width), (int)(frac * origSize.height));
-	}
+            } finally {
+                g2d.dispose();
+            }
+        }
+    }
+
+    private Dimension trimSize(Dimension origSize, Dimension maxSize) {
+        if ((origSize.width < maxSize.width) && (origSize.height < maxSize.height)) {
+            return origSize;
+        }
+
+        double hFrac = (double)maxSize.width / (double)origSize.width;
+        double vFrac = (double)maxSize.height/ (double)origSize.height;
+
+        double frac = (hFrac < vFrac) ? hFrac : vFrac;
+
+        return new Dimension((int)(frac * origSize.width), (int)(frac * origSize.height));
+    }
 }
--- src/com/mebigfatguy/polycasso/DefaultScore.java
+++ src/com/mebigfatguy/polycasso/DefaultScore.java
@@ -6,50 +6,51 @@ package com.mebigfatguy.polycasso;
  * then rolls up these scores to an overall score
  */
 public class DefaultScore implements Score {
-    
+
     static final int NUM_DIVISIONS = 4;
-    
+
     /**
      * a worst case score
      */
     public static final DefaultScore MAX_SCORE = new DefaultScore(Long.MAX_VALUE);
-    
+
     long gridScores[][] = new long[NUM_DIVISIONS][NUM_DIVISIONS];
     long overallScore;
-    
+
     /**
      * constructs an empty score
      */
     public DefaultScore() {
         overallScore = 0;
     }
-    
-    /** 
+
+    /**
      * constructs a score with the specified delta, spreads the score across all grids
      * 
      * @param delta the delta score
      */
     public DefaultScore(long delta) {
         overallScore = delta;
-        
+
         long divider = NUM_DIVISIONS * NUM_DIVISIONS;
         long gridScore = overallScore / divider;
-        
+
         for (int y = 0; y < NUM_DIVISIONS; y++) {
             for (int x = 0; x < NUM_DIVISIONS; x++) {
                 gridScores[x][y] = gridScore;
             }
         }
     }
-    
+
     /**
      * returns the sum of the square of pixel errors
      * @return the delta between a generate image and the target
      */
+    @Override
     public long getDelta() {
         return overallScore;
     }
-    
+
     /**
      * compares this core to another
      * 
@@ -58,12 +59,13 @@ public class DefaultScore implements Score {
      */
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof DefaultScore))
+        if (!(o instanceof DefaultScore)) {
             return false;
-        
+        }
+
         return getDelta() == ((DefaultScore) o).getDelta();
     }
-    
+
     /**
      * clones this object
      * 
@@ -72,22 +74,22 @@ public class DefaultScore implements Score {
     @Override
     public Object clone() {
         DefaultScore clonedScore;
-        
+
         try {
             clonedScore = (DefaultScore) super.clone();
         } catch (CloneNotSupportedException cnse) {
             clonedScore = new DefaultScore();
             clonedScore.overallScore = overallScore;
         }
-        
+
         clonedScore.gridScores = new long[NUM_DIVISIONS][NUM_DIVISIONS];
         for (int i = 0; i < NUM_DIVISIONS; i++) {
             System.arraycopy(gridScores[i], 0, clonedScore.gridScores[i], 0, NUM_DIVISIONS);
         }
-        
+
         return clonedScore;
     }
-    
+
     /**
      * generates a hash code for this score
      * 
@@ -97,7 +99,7 @@ public class DefaultScore implements Score {
     public int hashCode() {
         return (int) overallScore;
     }
-    
+
     /**
      * returns a string representation of the score
      * 
