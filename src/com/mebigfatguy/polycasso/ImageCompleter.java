98edd1a8fc437dad47b45057d673c4bcb3d855aa
Dave Brosius <dbrosius@mebigfatguy.com>
2013-07-12 22:22:05 -0400

    update dates

--- src/com/mebigfatguy/polycasso/ImageCompleter.java
+++ src/com/mebigfatguy/polycasso/ImageCompleter.java
@@ -1,7 +1,7 @@
 /*
  * polycasso - Cubism Artwork generator
- * Copyright 2009-2012 MeBigFatGuy.com
- * Copyright 2009-2012 Dave Brosius
+ * Copyright 2009-2013 MeBigFatGuy.com
+ * Copyright 2009-2013 Dave Brosius
  * Inspired by work by Roger Alsing
  *  
  * Licensed under the Apache License, Version 2.0 (the "License"); 
