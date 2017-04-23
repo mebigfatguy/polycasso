e8a96a7e0960f8a0df6aca168b9b1200efdb99c4
Dave Brosius <dbrosius@mebigfatguy.com>
2017-04-22 21:10:16 -0400

    switch to using dogpile image search

--- src/com/mebigfatguy/polycasso/RandomImageFinder.java
+++ src/com/mebigfatguy/polycasso/RandomImageFinder.java
@@ -3,13 +3,13 @@
  * Copyright 2009-2017 MeBigFatGuy.com
  * Copyright 2009-2017 Dave Brosius
  * Inspired by work by Roger Alsing
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,6 @@ package com.mebigfatguy.polycasso;
 
 import java.awt.Image;
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -34,9 +33,11 @@ import javax.swing.ImageIcon;
  */
 public class RandomImageFinder {
 
-    private static final String ROOTURL = "http://search.lycos.com";
-    private static final String URL = ROOTURL + "/images/?q={0}";
-    private static final Pattern IMAGE_HTML_PATTERN = Pattern.compile("\"ClickURL\":\"([^\"]*)\"");
+    private static final String ROOTURL = "http://www.dogpile.com";
+    private static final String URL = ROOTURL + "/search/images?q=%s";
+    private static final Pattern IMAGE_HTML_PATTERN = Pattern
+            .compile("\\<div class=\"resultDisplayUrl\"\\>\\s*\\<a data-icl-coi=\"\\d*\" data-icl-cop=\"results-main\" href=\"([^\"]+)");
+
     private static final int NAMELEN = 3;
     private static final int ATTEMPTS = 5;
 
@@ -47,11 +48,14 @@ public class RandomImageFinder {
     }
 
     /**
-     * finds an image thru lycos image search
-     * @param settings settings to fetch proxy information from
+     * finds an image thru dogpile image search
+     *
+     * @param settings
+     *            settings to fetch proxy information from
      * @return a random image
-     * 
-     * @throws IOException if fetching resources at urls fails
+     *
+     * @throws IOException
+     *             if fetching resources at urls fails
      */
     public static Image findImage(Settings settings) throws IOException {
 
@@ -60,10 +64,10 @@ public class RandomImageFinder {
                 char[] ranName = new char[NAMELEN];
                 Random ran = new Random(System.currentTimeMillis());
                 for (int c = 0; c < NAMELEN; c++) {
-                    ranName[c] = (char)('A' + ran.nextInt(26));
+                    ranName[c] = (char) ('A' + ran.nextInt(26));
                 }
 
-                return findImageAt(MessageFormat.format(URL, new String(ranName)), settings);
+                return findImageAt(String.format(URL, new String(ranName)), settings);
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             }
@@ -73,26 +77,29 @@ public class RandomImageFinder {
     }
 
     /**
-     * retrieve an image thru lycos image search
-     * 
-     * @param url the url to look for the image
-     * @param settings settings to fetch proxy information from
+     * retrieve an image thru dogpile image search
+     *
+     * @param url
+     *            the url to look for the image
+     * @param settings
+     *            settings to fetch proxy information from
      * @return the image
-     * @throws IOException if the site that is hosting the image is down, or non responsive
+     * @throws IOException
+     *             if the site that is hosting the image is down, or non responsive
      */
     private static final Image findImageAt(String url, Settings settings) throws IOException {
 
         String html = new String(URLFetcher.fetchURLData(url, settings.getProxyHost(), settings.getProxyPort()), "UTF-8");
 
-        List<String> images = new ArrayList<String>();
+        List<String> images = new ArrayList<>();
 
         Matcher m = IMAGE_HTML_PATTERN.matcher(html);
         while (m.find()) {
             images.add(m.group(1));
         }
 
-        String imageURL = images.get((int)(Math.random() * images.size()));
-        imageURL = imageURL.replace("\\",  "");
+        String imageURL = images.get((int) (Math.random() * images.size()));
+        imageURL = imageURL.replace("\\", "");
         return new ImageIcon(URLFetcher.fetchURLData(imageURL, settings.getProxyHost(), settings.getProxyPort())).getImage();
     }
 }
