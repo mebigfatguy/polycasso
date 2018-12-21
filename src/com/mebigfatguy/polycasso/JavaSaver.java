d09f9e038e4e61e500ae0b38d364d9fa2f639855
Dave Brosius <dbrosius@mebigfatguy.com>
2018-12-20 23:33:43 -0500

    remove deprecations

--- src/com/mebigfatguy/polycasso/JavaSaver.java
+++ src/com/mebigfatguy/polycasso/JavaSaver.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
 
@@ -36,146 +37,145 @@ import org.apache.commons.io.IOUtils;
  */
 public class JavaSaver implements Saver {
 
-    private static final String EXTENSION = ".java";
-    private static final String TABS = "\t\t\t\t\t";
+	private static final String EXTENSION = ".java";
+	private static final String TABS = "\t\t\t\t\t";
 
-    /**
-     * saves the polygon data as a java file that opens a JFrame and draws the polygons
-     *
-     * @param fileName
-     *            the name of the file to write to
-     * @param imageSize
-     *            the dimension of the image
-     * @param data
-     *            the polygons to draw
-     */
-    @Override
-    public void save(String fileName, Dimension imageSize, PolygonData[] data) throws IOException {
+	/**
+	 * saves the polygon data as a java file that opens a JFrame and draws the
+	 * polygons
+	 *
+	 * @param fileName  the name of the file to write to
+	 * @param imageSize the dimension of the image
+	 * @param data      the polygons to draw
+	 */
+	@Override
+	public void save(String fileName, Dimension imageSize, PolygonData[] data) throws IOException {
 
-        int sep = fileName.lastIndexOf(File.separator);
-        String className;
-        if (sep >= 0) {
-            className = fileName.substring(sep + 1);
-        } else {
-            className = fileName;
-        }
+		int sep = fileName.lastIndexOf(File.separator);
+		String className;
+		if (sep >= 0) {
+			className = fileName.substring(sep + 1);
+		} else {
+			className = fileName;
+		}
 
-        if (className.endsWith(EXTENSION)) {
-            className = className.substring(0, className.length() - EXTENSION.length());
-        }
+		if (className.endsWith(EXTENSION)) {
+			className = className.substring(0, className.length() - EXTENSION.length());
+		}
 
-        try (PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
-                InputStream templateStream = getClass().getResourceAsStream("/com/mebigfatguy/polycasso/JavaSaver.template")) {
-            String template = IOUtils.toString(templateStream);
+		try (PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+				InputStream templateStream = getClass()
+						.getResourceAsStream("/com/mebigfatguy/polycasso/JavaSaver.template")) {
+			String template = IOUtils.toString(templateStream, StandardCharsets.UTF_8);
 
-            String polygonData = getPolygonData(data);
-            String colorData = getColorData(data);
-            String transparencyData = getTransparencyData(data);
+			String polygonData = getPolygonData(data);
+			String colorData = getColorData(data);
+			String transparencyData = getTransparencyData(data);
 
-            /* All the curly braces confuses MessageFormat, so just do it manually */
-            template = template.replaceAll("\\{0\\}", className);
-            template = template.replaceAll("\\{1\\}", String.valueOf(imageSize.width));
-            template = template.replaceAll("\\{2\\}", String.valueOf(imageSize.height));
-            template = template.replaceAll("\\{3\\}", polygonData);
-            template = template.replaceAll("\\{4\\}", colorData);
-            template = template.replaceAll("\\{5\\}", transparencyData);
+			/* All the curly braces confuses MessageFormat, so just do it manually */
+			template = template.replaceAll("\\{0\\}", className);
+			template = template.replaceAll("\\{1\\}", String.valueOf(imageSize.width));
+			template = template.replaceAll("\\{2\\}", String.valueOf(imageSize.height));
+			template = template.replaceAll("\\{3\\}", polygonData);
+			template = template.replaceAll("\\{4\\}", colorData);
+			template = template.replaceAll("\\{5\\}", transparencyData);
 
-            pw.println(template);
+			pw.println(template);
 
-        } catch (IOException ioe) {
-        }
-    }
+		} catch (IOException ioe) {
+		}
+	}
 
-    private String getPolygonData(PolygonData... data) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
+	private String getPolygonData(PolygonData... data) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
 
-        String outerComma = "";
-        String innerComma;
-        for (PolygonData pd : data) {
+		String outerComma = "";
+		String innerComma;
+		for (PolygonData pd : data) {
 
-            pw.println(outerComma);
-            pw.print(TABS);
-            pw.println("{");
+			pw.println(outerComma);
+			pw.print(TABS);
+			pw.println("{");
 
-            /* Xs */
-            pw.print(TABS);
-            pw.print("\t{");
+			/* Xs */
+			pw.print(TABS);
+			pw.print("\t{");
 
-            innerComma = "";
-            Polygon poly = pd.getPolygon();
-            for (int j = 0; j < poly.npoints; j++) {
-                pw.print(innerComma);
-                pw.print(poly.xpoints[j]);
-                innerComma = ",";
-            }
+			innerComma = "";
+			Polygon poly = pd.getPolygon();
+			for (int j = 0; j < poly.npoints; j++) {
+				pw.print(innerComma);
+				pw.print(poly.xpoints[j]);
+				innerComma = ",";
+			}
 
-            pw.println("},");
+			pw.println("},");
 
-            /* Ys */
-            pw.print(TABS);
-            pw.print("\t{");
+			/* Ys */
+			pw.print(TABS);
+			pw.print("\t{");
 
-            innerComma = "";
-            poly = pd.getPolygon();
-            for (int j = 0; j < poly.npoints; j++) {
-                pw.print(innerComma);
-                pw.print(poly.ypoints[j]);
-                innerComma = ",";
-            }
+			innerComma = "";
+			poly = pd.getPolygon();
+			for (int j = 0; j < poly.npoints; j++) {
+				pw.print(innerComma);
+				pw.print(poly.ypoints[j]);
+				innerComma = ",";
+			}
 
-            pw.println("}");
+			pw.println("}");
 
-            pw.print(TABS);
-            pw.print("}");
-            outerComma = ",";
-        }
+			pw.print(TABS);
+			pw.print("}");
+			outerComma = ",";
+		}
 
-        pw.flush();
-        return sw.toString();
-    }
+		pw.flush();
+		return sw.toString();
+	}
 
-    private String getColorData(PolygonData[] data) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
+	private String getColorData(PolygonData[] data) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
 
-        String comma = "";
-        for (PolygonData pd : data) {
+		String comma = "";
+		for (PolygonData pd : data) {
 
-            pw.println(comma);
-            pw.print(TABS);
-            pw.print("{");
+			pw.println(comma);
+			pw.print(TABS);
+			pw.print("{");
 
-            Color color = pd.getColor();
-            pw.print(color.getRed());
-            pw.print(",");
-            pw.print(color.getGreen());
-            pw.print(",");
-            pw.print(color.getBlue());
-            pw.print("}");
-            comma = ",";
-        }
+			Color color = pd.getColor();
+			pw.print(color.getRed());
+			pw.print(",");
+			pw.print(color.getGreen());
+			pw.print(",");
+			pw.print(color.getBlue());
+			pw.print("}");
+			comma = ",";
+		}
 
-        pw.flush();
-        return sw.toString();
-    }
+		pw.flush();
+		return sw.toString();
+	}
 
-    private String getTransparencyData(PolygonData[] data) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        pw.println();
-        pw.print(TABS);
-        String comma = "";
-        for (PolygonData pd : data) {
+	private String getTransparencyData(PolygonData[] data) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		pw.println();
+		pw.print(TABS);
+		String comma = "";
+		for (PolygonData pd : data) {
 
-            pw.print(comma);
-            pw.print(pd.getAlpha());
-            pw.print("f");
-            comma = ",";
-        }
+			pw.print(comma);
+			pw.print(pd.getAlpha());
+			pw.print("f");
+			comma = ",";
+		}
 
-        pw.flush();
-        return sw.toString();
-    }
+		pw.flush();
+		return sw.toString();
+	}
 
 }
